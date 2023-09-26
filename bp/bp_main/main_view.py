18e039b629c6bfdb160cf5b2b8cb091ea29faad2
GilmanovAS <70641821+GilmanovAS@users.noreply.github.com>
2023-09-26 22:20:47 +0500

    8 step done without test need to do exercise star and decorator

--- bp/bp_main/main_view.py
+++ bp/bp_main/main_view.py
@@ -8,22 +8,17 @@ from configs.config import PATH_JSON_POSTS, PATH_JSON_COMMENTS
 blueprint_main = Blueprint('blueprint_main', __name__, template_folder='templates_main')
 
 
-def decor_add_a_tag(func):
-    def wrapped(st):
-        return f'<а href="/tag/{st}">#{st}</a>'
-
-    return wrapped
+def add_a_tag(st):
+    return f'<a href="/tag/{st[1:]}">{st}</a>'
 
 
-@decor_add_a_tag
-def add_a_tag(tag_name):
-    return tag_name
-
-
-def change_tag_content(all_posts):
-    for post1 in all_posts:
-        print(post1.get('content'))
-
+def change_tag_content(post):
+    content = post['content'].split()
+    for item in enumerate(content):
+        if '#' == item[1][0]:
+            content[item[0]] = add_a_tag(content[item[0]])
+    post['content'] = ' '.join(content)
+    return post
 
 
 @blueprint_main.route('/', methods=['GET'])
@@ -39,11 +34,9 @@ def post_page(post_id):
     post = DaoPosts(PATH_JSON_POSTS)
     comments_o = DaoComments(PATH_JSON_COMMENTS)
     comments_l, comments_count = comments_o.get_comments_by_post_id(post_id)
-    list_all = []
-    list_all = post.get_post_by_pk(post_id)
-
-    change_tag_content(list_all)
-    return render_template('post.html', post=list_all,
+    post = post.get_post_by_pk(post_id)
+    post = change_tag_content(post)
+    return render_template('post.html', post=post,
                            comments=comments_l, comments_count=comments_count)
 
 
@@ -70,3 +63,7 @@ def search_page_request():
     logging.info(f"/search/?search={search_str}")
     posts_l, count = posts_o.search_for_posts(search_str)
     return render_template('search.html', posts=posts_l, count=count)
+
+@blueprint_main.route('/tag/<tagname>')
+def tag_page(tagname):
+    return render_template('tag.html')
